4056289eefd4a04e9314b765e6630051f9e50080
dinesh-thurpati <dinesheshan.852@gmail.com>
2025-01-26 19:03:13 +0530

    Add files via upload

---
+++ oops.js
@@ -0,0 +1,41 @@
+// // class person{
+// //     constructor(){
+// //         this.pname = "im dinesh.how are you ?"
+// //     }
+// // }
+
+// // let p1 = new person{
+// //     console.log(p1)
+// // }
+// // let p2 = new person {
+// //     console.log(p2)
+// // }
+
+
+// class numbersystem{
+//     constructor(){
+//         this.num = 20
+//     }
+// }
+
+// checkEven(){
+//     if(this.num%2===0){
+//         console.log ("number is even")
+//     }
+//       else{
+//         console.log("number is odd")
+//       }  
+// }
+
+// let n1 = numbersystem(22)
+import React from "react";
+import ReactDOM from "react-dom/client";
+function Hello(props) {
+    return <h1>Hello World!</h1>;
+  }
+  
+  const container = document.getElementById('root');
+  const root = ReactDOM.createRoot(container);
+  root.render(<Hello />);
+
+
